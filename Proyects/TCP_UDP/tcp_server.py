3662dba0c142c9b1d828d5c74a76954986a57d75
Luis Serrano <31300266+LuisS96@users.noreply.github.com>
2018-07-27 21:16:09 -0700

    Crete tcp_server.py
    It is just an example.

---
+++ Proyects/TCP_UDP/tcp_server.py
@@ -0,0 +1,25 @@
+# Import Libraries
+import socket
+
+# Define IP Address, Port, and Buffer Size
+TCP_IP = '127.0.0.1'
+TCP_PORT = 5005
+BUFFER_SIZE = 20  # Normally 1024, but we want fast response
+
+# Create the TCP between the server and the client
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+
+conn, addr = s.accept()
+print 'Connection address:', addr
+
+# Make a loop to receive and send packets to the client
+while 1:
+    data = conn.recv(BUFFER_SIZE)
+    if not data: break
+    print "received data:", data
+    conn.send(data)  # echo
+
+# Close communication with the client
+conn.close()
